bbad3a2097e49ea74018d04d87bb4544c3b7efce
richiivv <richi.varela323@gmail.com>
2025-04-05 15:23:43 -0600

    main.c

--- main.c
+++ main.c
@@ -4,17 +4,12 @@
 #include "menu.h"
 
 int main(){
-int opcion,max_articulos,opc2;
+int opcion,opc2,opc3,opc4;
 int num_articulos=0;
 
+Inventario *arreglo;
 
-Arreglo_articulos *arreglo;
-
-printf("\n Introduzca el numero maximo de articulos a manejar:");
-scanf("%d",&max_articulos);
-
-arreglo=crearArreglo(max_articulos);
-LeerArticulos(arreglo,&num_articulos);
+arreglo=crearArreglo();
 
     do {
         printf("\n");
@@ -22,39 +17,56 @@ LeerArticulos(arreglo,&num_articulos);
         switch (opcion){
             case 1:
             do{
-
-            opc2=desplegarMenu("\n1)Insertar art. \n2)Listar articulos. \n3)Buscar por codigo \n4)Actualizar info. \n5)Borrar art. \n6)Gestionar inventario \n7)Salir  ",7);
+                
+            opc2=desplegarMenu("\n1)Insertar art. \n2)Listar articulos. \n3)Borrar art. \n4)Actualizar info. \n5)Buscar por codigo. \n6)Gestionar inventario \n7)Salir  ",7);
             switch (opc2){
                 case 1:
-                insertarArticulo(num_articulos,max_articulos,arreglo);
-                num_articulos++;
+                if(arreglo->num_departamentos==0){
+                    crearDepartamento(arreglo);
+                    insertarArticulo(0,arreglo->departamentos[0].num_articulos,arreglo);
+                }else{
+                    printf("\n Seleccione el departamento en el cual se agregara el articulo");
+                    for(int i=0;i<arreglo->num_departamentos;i++){
+                        printf("Dep.[%d]:%s\n",i,arreglo->departamentos[i].nombre_departamento);
+                    }
+                    scanf("%d",&opc3);
+                    insertarArticulo(opc3,arreglo->departamentos[opc3].num_articulos,arreglo);
+                    
+                }
                 break;
-                case 2:
-                listarArticulos(num_articulos,*arreglo);
-                break;
+            
+            case 2:
+            if(arreglo->num_departamentos==0){
+                printf("\n ERROR: no hay departamentos registrados...");
+            }else{
+                printf("\n Seleccione el departamento del cual se listaran los articulos");
+                for(int i=0;i<arreglo->num_departamentos;i++){
+                        printf("Dep.[%d]:%s\n",i,arreglo->departamentos[i].nombre_departamento);
+                    }
+                scanf("%d",&opc4);
+                listarArticulos(opc4,arreglo->departamentos[opc4].num_articulos,arreglo);
+            }
+            break;
+            
             }
             }while(opc2 !=7);
                 break;
-
-            case 2:
 
                     break;
 
             case 3:
                 break;
         }
-
     } while (opcion !=4);
-}
+} 
+/*Insertar un artÃ­culo nuevo
 
-/*Insertar un artículo nuevo
+Eliminar un artÃ­culo
 
-Eliminar un artículo
+Buscar un artÃ­culo por su cÃ³digo
 
-Buscar un artículo por su código
+Actualizar informaciÃ³n de un artÃ­culo
 
-Actualizar información de un artículo
-
-Listar todos los artículos
+Listar todos los artÃ­culos
 
 Gestionar inventarios (aumentar o disminuir inventario)*/
