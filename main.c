bad6039f426db11afce5f0f5f9c0a52808cc310c
ErickR1305 <erickmunoz13052006@gmail.com>
2025-04-06 15:22:48 -0600

    Update main.c
    Agregar leerArchivo

--- main.c
+++ main.c
@@ -7,7 +7,7 @@ int main(){
 int opcion,opc2,opc3,opc4;
 int num_articulos; //indice
 int max_articulos; //max-1, para que no se pase de lo asignado por malloc
-int codigo;
+int codigo,i;
 
 Articulo *arreglo;
 printf("\n Ingrese la cantidad maxima de articulos a manejar:");
@@ -16,47 +16,52 @@ scanf("%d",&max_articulos);
 arreglo=crearArreglo(max_articulos);
 num_articulos=0;
 
+LeerArticulos(arreglo,&num_articulos);
     do {
         printf("\n");
         opcion = desplegarMenu("\n1)Organizar inventario \n2)Atender a un cliente \n3)Estadisticas \n4)Salir\nOpcion: ",4);
         switch (opcion){
             case 1:
             do{
-                
+
             opc2=desplegarMenu("\n1)Insertar art. \n2)Listar articulos \n3)Buscar articulos \n4)Borrar art. \n5)Actualizar info. \n6)Salir ",6);
             switch (opc2){
                 case 1:
                 insertarArticulo(&num_articulos,max_articulos,arreglo);
                 break;
-            
+
             case 2:
             if(num_articulos==0){
                 printf("\n ERROR: no hay articulos registrados...");
             }else{
-                listarArticulos(num_articulos,arreglo);
+                i=0;//se reinicia para que liste todos
+                printf("\n ====ARTICULOS REGISTRADOS====");
+                while(i<num_articulos)
+                listarArticulos(&i,arreglo);
+
             }
             break;
-            
+
             case 3:
             if(num_articulos==0){
                 printf("\n ERROR: no hay articulos registrados...");
             }else{
-                printf("Introduzca el codigo del producto a buscar");
+                printf("Introduzca el codigo del producto a buscar: ");
                 scanf("%d",&codigo);
                 buscarArticulos(codigo,arreglo,num_articulos);
             }
             break;
-            
+
             case 4:
             if(num_articulos==0){
                 printf("\n ERROR: no hay articulos registrados...");
             }else{
-                printf("Introduzca el codigo del producto a eliminar");
+                printf("Introduzca el codigo del producto a eliminar: ");
                 scanf("%d",&codigo);
             borrarArticulo(codigo,arreglo,&num_articulos);
             }
             break;
-            
+
             case 5:
             if(num_articulos==0){
                 printf("\n ERROR: no hay articulos registrados...");
@@ -66,10 +71,10 @@ num_articulos=0;
                 actualizarArticulo(codigo,arreglo,num_articulos);
             }
             break;
-            
+
             case 6:
             printf("Saliendo del menu:Organizar inventario... Volviendo al menu principal");
-            
+
             }
             }while(opc2 !=6);
                 break;
@@ -80,4 +85,4 @@ num_articulos=0;
                 break;
         }
     } while (opcion !=4);
-} 
+}
