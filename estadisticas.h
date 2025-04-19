b6feed528d65c7ecbbc16e7b9665e4582163220a
Enrique <enmimica1@gmail.com>
2025-04-18 23:54:56 -0600

    Create estadisticas.h

---
+++ estadisticas.h
@@ -0,0 +1,20 @@
+#ifndef ESTADISTICAS_H
+#define ESTADISTICAS_H
+
+#include "colaCircular.h"
+#include "inventario.h"
+
+// Prototipos de funciones
+int departamentoMenorInventario(Articulo *arreglo, int num_articulos);
+int departamentoMayorVenta(Articulo *arreglo, int num_articulos);
+Articulo articuloMasCaro(Articulo *arreglo, int num_articulos);
+void totalVentasPorDepartamento(Articulo *arreglo, int num_articulos);
+Articulo productoMasVendido(Articulo *arreglo, int num_articulos);
+void porcentajeInventarioVendido(Articulo *arreglo, int num_articulos);
+float promedioPrecio(Articulo *arreglo, int num_articulos);
+void listarProductosPorPrecio(Articulo *arreglo, int num_articulos);
+int totalInventarioDisponible(Articulo *arreglo, int num_articulos);
+Articulo productoMenorInventario(Articulo *arreglo, int num_articulos);
+void mostrarEstadisticas(ColaCircular *cola, Articulo *arreglo, int num_articulos);
+
+#endif
