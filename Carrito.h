e2593ff1f2a633bbfb39ac37fc027c5aa6a1edeb
ErickR1305 <erickmunoz13052006@gmail.com>
2025-04-18 19:25:43 -0600

    Update Carrito.h
    Act

--- Carrito.h
+++ Carrito.h
@@ -3,7 +3,7 @@
 #include "inventario.h"
 
 Articulo *CrearArticulo();
-void CapturarArticulo(Articulo *unArticulo,Articulo *Arreglo,int num,float *cont);
+void CapturarArticulo(Articulo *unArticulo,Articulo *Arreglo,int num);
 void ListarArticulo(Articulo unArticulo);
 void LiberarArticulo(Articulo *unArticulo);
 
